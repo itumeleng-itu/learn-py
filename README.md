503bf178be921dc7983605110bf85988331c2815
itumeleng-itu <matomempaola@gmail.com>
2025-07-24 08:06:00 +0200

    hello python

---
+++ README.md
@@ -0,0 +1,4 @@
+## ABOUT FILE
+
+>this file depicts a python file that prompts a user for their name and surname and then later responds with a proper greeting 
+>file name is hello.py
---
+++ hello.py
@@ -0,0 +1,8 @@
+print("Welcome! Let's get to know each other.\n")
+
+first_name = input("What is your first name? ")
+
+surname = input("What is your surname? ")
+
+print(f"\nHello, {first_name} {surname}! It's nice to meet you.")
+print("Thank you for using this program. Have a great day!")
